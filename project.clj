d0828f9049460ed91c95c30fc9d11a4fa79eca7d
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2015-03-13 10:13:10 +0200

    minimal JSON REST API setup

--- project.clj
+++ project.clj
@@ -3,7 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :ring {:handler compojure-example.core/app}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.2.0"]
+                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-json "0.3.1"]
+                 [ring/ring-jetty-adapter "1.3.2"]]
+  :plugins [[lein-ring "0.8.13"]]
   :main ^:skip-aot compojure-example.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :aot :all
+  :profiles
+   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}})
--- src/compojure_example/core.clj
+++ src/compojure_example/core.clj
@@ -1,7 +1,25 @@
 (ns compojure-example.core
-  (:gen-class))
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.json :as middleware]
+            [ring.adapter.jetty :as jetty]
+            [compojure.handler :as handler]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- get-article [id]
+	{:body {:body "<p>body text</p>"
+	        :id id
+	        :url (str "http://www.newscorp.com/articles/" id)}
+	 :headers {"Cache-Control" "no-transform,public,max-age=10"}})
+
+(defroutes app-routes
+  (GET "/api/v1/article/:id" [id] (get-article id)) 
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (handler/api app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-params)
+      (middleware/wrap-json-response)))
+
+(defn -main [& args]
+  (jetty/run-jetty app))
