de593995b2a70ffcd3421cc08a833af542492110
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2015-03-13 10:02:15 +0200

    lein new app compojure-example

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to compojure-example
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/compojure_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns compojure-example.core-test
+  (:require [clojure.test :refer :all]
+            [compojure-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
